02347f13cce7b7996b560940a41d1f8b341730d6
seanpue <pue@msu.edu>
2019-07-28 13:42:56 -0400

    latex docs error fix attempt

--- docs/conf.py
+++ docs/conf.py
@@ -107,15 +107,15 @@ htmlhelp_basename = 'graphtransliteratordoc'
 
 
 # -- Options for LaTeX output ------------------------------------------
-
+latex_engine = 'xelatex'
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
-    # 'papersize': 'letterpaper',
+     'papersize': 'letterpaper',
 
     # The font size ('10pt', '11pt' or '12pt').
     #
-    # 'pointsize': '10pt',
+     'pointsize': '10pt',
 
     # Additional stuff for the LaTeX preamble.
     #
