6a238d4d72548ab7e54bf2190eec64f31947b0cb
seanpue <pue@msu.edu>
2019-08-15 13:22:31 -0400

    Bump version: 0.2.13 → 0.2.14

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/seanpue/graphtransliterator",
-    version="0.2.13",
+    version="0.2.14",
     zip_safe=False,
 )
