27f3b0ab744f6942bf433198df1ecaaa09ce0d97
seanpue <pue@msu.edu>
2020-04-21 16:23:08 -0400

    Bump version: 1.1.0 → 1.1.1

--- graphtransliterator/__init__.py
+++ graphtransliterator/__init__.py
@@ -16,7 +16,7 @@ convert between transliteration systems.
 
 __author__ = """A. Sean Pue"""
 __email__ = "pue@msu.edu"
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 # Core classes
 from .core import CoverageTransliterator, GraphTransliterator, GraphTransliteratorSchema
